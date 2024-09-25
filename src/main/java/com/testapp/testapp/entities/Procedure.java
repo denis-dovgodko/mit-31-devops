06f89add4edf53df90a395b03dd797bdd3690489
denis.dovgodko <dendov555@gmail.com>
2024-09-25 18:14:44 +0300

    lab2 done(tests)

---
+++ src/main/java/com/testapp/testapp/entities/Procedure.java
@@ -0,0 +1,31 @@
+package com.testapp.testapp.entities;
+
+public class Procedure {
+    private String procedureName;
+    private Integer durationTimeMinutes;
+
+    public Procedure(String procedureName, Integer durationTimeMinutes) {
+        this.procedureName = procedureName;
+        this.durationTimeMinutes = durationTimeMinutes;
+    }
+
+    public Integer getDurationTimeMinutes() {
+        return durationTimeMinutes;
+    }
+
+    @Override
+    public String toString() {
+        return procedureName;
+    }
+    public void setProcedureName(String procedureName) {
+        this.procedureName = procedureName;
+    }
+//    public String getProcedureName() {
+//        return procedureName;
+//    }
+//
+//
+//    public void setDurationTimeMinutes(Integer durationTimeMinutes) {
+//        this.durationTimeMinutes = durationTimeMinutes;
+//    }
+}
